9b6407cce441ef1c90caacfa1184e0e8a7243664
venkibellu <venkateshrbelavadi@gmail.com>
2017-09-06 23:48:00 +0530

    Buggy Push

--- app/src/main/java/com/uvce/bestbuddy/MainActivity.java
+++ app/src/main/java/com/uvce/bestbuddy/MainActivity.java
@@ -11,11 +11,15 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     FirebaseDatabase firebaseDatabase;
     DatabaseReference ref;
     EditText dairyInput;
+    String numberOfPosts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
         dairyInput=(EditText)findViewById(R.id.dairy_input);
 
         firebaseDatabase=FirebaseDatabase.getInstance();
-        ref=firebaseDatabase.getReference();
+        ref=firebaseDatabase.getReference().child("Testuser");
 
 
     }
@@ -49,7 +54,26 @@ public class MainActivity extends AppCompatActivity {
     public void submitDairy(View v)
     {
         String currentDateTimeString = DateFormat.getDateTimeInstance().format(new Date());
-        ref.child("Testuser").child(currentDateTimeString).setValue(dairyInput.getText().toString().trim());
+        numberOfPosts="5";
+        ref.child("Post Count").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                numberOfPosts=dataSnapshot.getValue(String.class);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+        HashMap hashMap=new HashMap();
+        hashMap.put("Date",currentDateTimeString);
+        hashMap.put("Record",dairyInput.getText().toString().trim());
+        hashMap.put("Score",0);
+
+
+        ref.child("Post "+numberOfPosts).setValue(hashMap);
+
         Toast.makeText(getApplicationContext(),"Your day has been saved",Toast.LENGTH_LONG).show();
         dairyInput.setText(null);
     }
