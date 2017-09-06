c8fc5c854b398602a23815e1fd83db94bd6623ac
venkibellu <venkateshrbelavadi@gmail.com>
2017-09-06 21:23:08 +0530

    Minor testing changes

--- app/src/main/java/com/uvce/bestbuddy/MainActivity.java
+++ app/src/main/java/com/uvce/bestbuddy/MainActivity.java
@@ -2,15 +2,30 @@ package com.uvce.bestbuddy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar=(Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater=getMenuInflater();
+        menuInflater.inflate(R.menu.menu_main,menu);
+        return true;
     }
 
     public void gotClicked(View v)
